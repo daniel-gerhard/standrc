29374f1e3759cd96bce1ee90b24576d0454996d4
daniel-gerhard <00gerhard@gamil.com>
2015-01-23 13:57:18 +1300

    added standrm_binary() for binary responses

--- R/standrm_binary.R
+++ R/standrm_binary.R
@@ -1,4 +1,4 @@
-standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standrc_priors(), ...){
+standrm_binary <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standrc_priors(), ...){
   callDetail <- match.call()
   
   mf <- model.frame(formula, data)
@@ -30,7 +30,7 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
     K <- 1
     pnlr <- rep(FALSE, 5)
   }
- 
+  
   fix <- fct$fixed
   if (fct$name %in% c("W1.4", "W1.3", "W2.4", "W2.3", "LN.4", "LN.3")) fix <- c(fix, 0)  
   isfix <- !is.na(fix)
@@ -39,18 +39,18 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
   jv[!isfix][!pnl] <- 1
   
   N <- nrow(mf) 
-  y <- mf[,1]
+  y <- as.integer(mf[,1])
   x <- mf[,2]   
   if (fct$name %in% c("LL.5", "LL.4", "LL.3", "W1.4", "W1.3", "W2.4", "W2.3", "LN.4", "LN.3")) x[x == 0] <- 0.5*min(x[x > 0])
   if (fct$name %in% c("LL.4", "LL.3", "L.4", "L.3")) fix[5] <- 0
   
   if (is.null(priors$pb)) pb <- rep(0, jv[1])  
   if (mean(y[x == min(x)]) < mean(y[x == max(x)])){
-    if (is.null(priors$pc)) pc <- rep(min(y), jv[2])
-    if (is.null(priors$pd)) pd <- rep(max(y), jv[3])
+    if (is.null(priors$pc)) pc <- rep(-10, jv[2])
+    if (is.null(priors$pd)) pd <- rep(10, jv[3])
   } else {
-    if (is.null(priors$pd)) pd <- rep(min(y), jv[3])
-    if (is.null(priors$pc)) pc <- rep(max(y), jv[2])
+    if (is.null(priors$pd)) pd <- rep(-10, jv[3])
+    if (is.null(priors$pc)) pc <- rep(10, jv[2])
   }  
   if (is.null(priors$pe)) pe <- rep(median(x), jv[4])
   if (is.null(priors$pf)) pf <- rep(0, jv[5])
@@ -68,7 +68,7 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
   dJp <- c("real pb[J];", "real pc[J];", "real pd[J];", "real pe[J];", "real pf[J];")[!isfix]
   dp <- c("real pb;", "real pc;", "real pd;", "real pe;", "real pf;")[!isfix]  
   dpc <- paste(sapply(1:length(pnl), function(i) if (pnl[i]) dJp[i] else dp[i]), collapse=" ")
-
+  
   # define parameters
   paraJ <- c("real slope[J];", "real lasy[J];", "real uasy[J];", "real<lower=0> ed[J];", "real assym[J];")[!isfix]
   para <- c("real slope;", "real lasy;", "real uasy;", "real<lower=0> ed;", "real assym;")[!isfix]
@@ -78,18 +78,18 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
   rpara <- c("real rslope[K];", "real rlasy[K];", "real ruasy[K];", "real red[K];", "real rassym[K];")
   rparasig <- c("real<lower=0> sigmasq_slope;", "real<lower=0> sigmasq_lasy;", "real<lower=0> sigmasq_uasy;", "real<lower=0> sigmasq_ed;", "real<lower=0> sigmasq_assym;")
   rparac <- paste(c(rpara[pnlr], rparasig[pnlr]), collapse=" ")
-   
-  trap <- c("slope", "lasy", "uasy", "ed", "assym")[!isfix]
-  traj <- c("slope[idc[i]]", "lasy[idc[i]]", "uasy[idc[i]]", "ed[idc[i]]", "assym[idc[i]]")[!isfix]
+  
+  trap <- c("slope", "1/(1 + exp(-1*lasy))", "1/(1 + exp(-1*uasy))", "ed", "assym")[!isfix]
+  traj <- c("slope[idc[i]]", "1/(1 + exp(-1*lasy[idc[i]]))", "1/(1 + exp(-1*uasy[idc[i]]))", "ed[idc[i]]", "assym[idc[i]]")[!isfix]
   tra <- character(length(fix))
   tra[!isfix] <- sapply(1:length(pnl), function(i) if (pnl[i]) traj[i] else trap[i])
   tra[isfix] <- fix[isfix]
   if (is.null(random)){
-    if (fct$name %in% c("LL.5", "LL.4", "LL.3")) trans <- paste("sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") / (1 + exp(-exp(", tra[1], ") * (log(x[i]/", tra[4], "))))^exp(", tra[5], ");}", collapse="")
-    if (fct$name %in% c("L.5", "L.4", "L.3")) trans <- paste("sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") / (1 + exp(-exp(", tra[1], ") * (x[i] - ", tra[4], ")))^exp(", tra[5], ");}", collapse="")
-    if (fct$name %in% c("W1.4", "W1.3")) trans <- paste("sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * exp(-exp(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], "))));}", collapse="")
-    if (fct$name %in% c("W2.4", "W2.3")) trans <- paste("sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * (1 - exp(-exp(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], ")))));}", collapse="")
-    if (fct$name %in% c("LN.4", "LN.3")) trans <- paste("sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * normal_cdf(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], ")), 0, 1);}", collapse="")
+    if (fct$name %in% c("LL.5", "LL.4", "LL.3")) trans <- paste("for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") / (1 + exp(-exp(", tra[1], ") * (log(x[i]/", tra[4], "))))^exp(", tra[5], ");}", collapse="")
+    if (fct$name %in% c("L.5", "L.4", "L.3")) trans <- paste("for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") / (1 + exp(-exp(", tra[1], ") * (x[i] - ", tra[4], ")))^exp(", tra[5], ");}", collapse="")
+    if (fct$name %in% c("W1.4", "W1.3")) trans <- paste("for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * exp(-exp(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], "))));}", collapse="")
+    if (fct$name %in% c("W2.4", "W2.3")) trans <- paste("for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * (1 - exp(-exp(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], ")))));}", collapse="")
+    if (fct$name %in% c("LN.4", "LN.3")) trans <- paste("for(i in 1:N){ mu[i] <-", tra[2], " + (", tra[3], "-", tra[2], ") * normal_cdf(-exp(", tra[1], ") * (log(x[i]) - log(", tra[4], ")), 0, 1);}", collapse="")
   } else {
     stra <- paste(c("real<lower=0> sigma_slope;", "real<lower=0> sigma_lasy;", "real<lower=0> sigma_uasy;", "real<lower=0> sigma_ed;", "real<lower=0> sigma_assym;")[pnlr], collapse=" ")
     strasq <- paste(c("sigma_slope <- sqrt(sigmasq_slope);",
@@ -98,16 +98,16 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
                       "sigma_ed <- sqrt(sigmasq_ed);",
                       "sigma_assym <- sqrt(sigmasq_assym);")[pnlr], collapse=" ")
     trar <- c(paste("(", tra[1], " + rslope[idr[i]] )"), 
-              paste("(", tra[2], " + rlasy[idr[i]] )"), 
-              paste("(", tra[3], " + ruasy[idr[i]] )"), 
+              paste("(1/(1 + exp(-1*(", tra[2], " + rlasy[idr[i]] )))"), 
+              paste("(1/(1 + exp(-1*(", tra[3], " + ruasy[idr[i]] )))"), 
               paste("(", tra[4], " + red[idr[i]] )"), 
               paste("(", tra[5], " + rassym[idr[i]] )"))
     trc <- sapply(1:length(tra), function(i) if (pnlr[i]) trar[i] else tra[i])    
-    if (fct$name %in% c("LL.5", "LL.4", "LL.3")) trans <- paste(stra, strasq, "sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") / (1 + exp(-exp(", trc[1], ") * (log(x[i]/", trc[4], "))))^exp(", trc[5], ");}", collapse="")
-    if (fct$name %in% c("L.5", "L.4", "L.3")) trans <- paste(stra, strasq, "sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") / (1 + exp(-exp(", trc[1], ") * (x[i] -", trc[4], ")))^exp(", trc[5], ");}", collapse="")
-    if (fct$name %in% c("W1.4", "W1.3")) trans <- paste(stra, strasq, "sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * exp(-exp(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], "))));}", collapse="")
-    if (fct$name %in% c("W2.4", "W2.3")) trans <- paste(stra, strasq, "sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * (1 - exp(-exp(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], ")))));}", collapse="")
-    if (fct$name %in% c("LN.4", "LN.3")) trans <- paste(stra, strasq, "sigma_y <- sqrt(sigmasq_y); for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * normal_cdf(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], ")), 0, 1);}", collapse="")
+    if (fct$name %in% c("LL.5", "LL.4", "LL.3")) trans <- paste(stra, strasq, "for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") / (1 + exp(-exp(", trc[1], ") * (log(x[i]/", trc[4], "))))^exp(", trc[5], ");}", collapse="")
+    if (fct$name %in% c("L.5", "L.4", "L.3")) trans <- paste(stra, strasq, "for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") / (1 + exp(-exp(", trc[1], ") * (x[i] -", trc[4], ")))^exp(", trc[5], ");}", collapse="")
+    if (fct$name %in% c("W1.4", "W1.3")) trans <- paste(stra, strasq, "for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * exp(-exp(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], "))));}", collapse="")
+    if (fct$name %in% c("W2.4", "W2.3")) trans <- paste(stra, strasq, "for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * (1 - exp(-exp(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], ")))));}", collapse="")
+    if (fct$name %in% c("LN.4", "LN.3")) trans <- paste(stra, strasq, "for(i in 1:N){ mu[i] <-", trc[2], " + (", trc[3], "-", trc[2], ") * normal_cdf(-exp(", trc[1], ") * (log(x[i]) - log(", trc[4], ")), 0, 1);}", collapse="")
   }
   
   ### population parameters
@@ -127,7 +127,7 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
   pof <- character(length(fix))
   pof[!isfix] <- sapply(1:length(pnl), function(i) if (pnl[i]) poj[!isfix][i] else po[!isfix][i])
   pof[isfix] <- po[isfix]
-  ppc <- paste(pof[1], " <- ", pp[1], if (pnlr[1]) " + rnslope", ";", pof[2], "<-", pp[2], if (pnlr[2]) " + rnlasy", ";", pof[3], " <- ", pp[3], if (pnlr[3]) " + rnuasy", ";", pof[4], " <- ", pp[4], if (pnlr[4]) " + rned", ";", pof[5], " <- ", pp[5], if (pnlr[5]) " + rnassym", ";")
+  ppc <- paste(pof[1], " <- ", pp[1], if (pnlr[1]) " + rnslope", ";", pof[2], " <- ", if (!isfix[2]) "1/(1 + exp(-1*(", pp[2], if (pnlr[2]) " + rnlasy", if (!isfix[2]) ")))", ";", pof[3], " <- ", if (!isfix[3]) "1/(1 + exp(-1*(", pp[3], if (pnlr[3]) " + rnuasy", if (!isfix[3]) ")))", ";", pof[4], " <- ", pp[4], if (pnlr[4]) " + rned", ";", pof[5], " <- ", pp[5], if (pnlr[5]) " + rnassym", ";")  
   
   modp <- paste(c("rnslope ~ normal(0, sigma_slope);", "rnlasy ~ normal(0, sigma_lasy);", "rnuasy ~ normal(0, sigma_uasy);", "rned ~ normal(0, sigma_ed);", "rnassym ~ normal(0, sigma_assym);")[pnlr], collapse=" ") 
   
@@ -139,7 +139,7 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
                  paste("ed ~", priors$e, ";"), 
                  paste("assym ~", priors$f, ";"))[!isfix], collapse=" ") 
   
-  mody <- paste("sigmasq_y ~", priors$sy, "; y ~ normal(mu, sigma_y);")
+  mody <- paste("y ~ bernoulli(mu);")
   
   modr <- paste(c("rslope ~ normal(0, sigma_slope);", "rlasy ~ normal(0, sigma_lasy);", "ruasy ~ normal(0, sigma_uasy);", "red ~ normal(0, sigma_ed);", "rassym ~ normal(0, sigma_assym);")[pnlr], collapse=" ") 
   modrsig <- paste(c(paste("sigmasq_slope ~", priors$sb, ";"), 
@@ -149,22 +149,25 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
                      paste("sigmasq_assym ~",priors$sf, ";"))[pnlr], collapse=" ") 
   
   # new random effects for derived ed
-  moded <- paste(c("rnslope ~ normal(0, sigma_slope);", "rnlasy ~ normal(0, sigma_lasy);", "rnuasy ~ normal(0, sigma_uasy);", "rned ~ normal(0, sigma_ed);", "rnassym ~ normal(0, sigma_assym);")[pnlr], collapse=" ") 
+  moded <- paste(c("rnslope ~ normal(0, sigma_slope);", 
+                   "rnlasy ~ normal(0, sigma_lasy);", 
+                   "rnuasy ~ normal(0, sigma_uasy);", 
+                   "rned ~ normal(0, sigma_ed);", 
+                   "rnassym ~ normal(0, sigma_assym);")[pnlr], collapse=" ") 
   
   stancode <- paste("data {", 
-                    "int<lower=0> N; real y[N];",
+                    "int<lower=0> N; int y[N];",
                     "real<lower=0> x[N];", 
                     if (!is.null(idc)) dJ,
                     if (!is.null(random)) dK,
                     dpc,
                     "} ",
-                    "parameters { real<lower=0> sigmasq_y;",
+                    "parameters {",
                     parac,
                     if (!is.null(random)) rparac, 
                     if (!is.null(random)) rnparac,  
                     "} ",
                     "transformed parameters {",
-                    "real<lower=0> sigma_y;",
                     "real mu[N];",                   
                     trans,                      
                     "} ",
@@ -180,8 +183,8 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
                     "real log_lik[N];",
                     popc,
                     "for (i in 1:N){",
-                        "residuals[i] <- y[i] - mu[i];",
-                        "log_lik[i] <- normal_log(y[i], mu[i], sigma_y);",
+                    "residuals[i] <- y[i] - mu[i];",
+                    "log_lik[i] <- bernoulli_log(y[i], mu[i]);",
                     "}",
                     ppc,                                     
                     "}",
@@ -204,4 +207,3 @@ standrm <- function(formula, data, fct, curveid=NULL, random=NULL, priors=standr
   class(out) <- "standrc"
   return(out)
 }
-
